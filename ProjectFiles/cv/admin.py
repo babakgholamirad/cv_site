ef717bacdf086cf010121c4eb1793120d59cf3b4
bagholamirad1999 <bagholamirad1999@gmail.com>
2020-12-19 19:35:08 +0330

    making Projects and User Model and AdminModels

--- ProjectFiles/cv/admin.py
+++ ProjectFiles/cv/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
+from .models import MemberOfProject, Project, User
+
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+class MembersInline(admin.TabularInline):
+    model = MemberOfProject
+    extra = 1
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = (MembersInline,)
+    list_display = [
+        'title',
+        'owner',
+        'shrot_description',
+        'start_date', ]
--- ProjectFiles/cv/models.py
+++ ProjectFiles/cv/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.text import Truncator
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=200, blank=True)
+    owner = models.ForeignKey('cv.User', on_delete=models.SET_NULL, null=True)
+    members = models.ManyToManyField(
+        'cv.User', through='MemberOfProject', related_name='member_projects')
+    description = models.TextField(null=True)
+    start_date = models.DateTimeField()
+
+    def __str__(self):
+        return(self.title)
+
+    def shrot_description(self):
+        return Truncator(self.description).words(20)
+
+
+class MemberOfProject(models.Model):
+    member = models.ForeignKey('cv.User', on_delete=models.CASCADE)
+    project = models.ForeignKey('cv.Project', on_delete=models.CASCADE)
+    role_in_project = models.CharField(max_length=100, null=False, blank=False)
+    date_joined = models.DateTimeField()
+    date_left = models.DateTimeField()
