d7560087b247b5b2bd3093dbac2725ec04b93ec6
babakgholamirad <bagholamirad@yahoo.com>
2022-04-12 01:49:27 +0430

    update 'force_text' module to 'force_str'
    Signed-off-by: babakgholamirad <bagholamirad@yahoo.com>

--- ProjectFiles/accounts/views.py
+++ ProjectFiles/accounts/views.py
@@ -8,7 +8,7 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.template import RequestContext
 from django.template.loader import render_to_string
-from django.utils.encoding import force_bytes, force_text
+from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
 from .forms import SignupForm
@@ -64,7 +64,7 @@ def signup(request):
 
 def activate(request, uidb64, token):
     try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
+        uid = force_str(urlsafe_base64_decode(uidb64))
         user = User.objects.get(pk=uid)
     except(TypeError, ValueError, OverflowError, User.DoesNotExist):
         user = None
