c1fa22a9c609481dddaacc264038523af9b677d6
babakgholamirad <bagholamirad@yahoo.com>
2022-04-16 10:10:28 +0430

    write some test with copilot
    Signed-off-by: babakgholamirad <bagholamirad@yahoo.com>

--- ProjectFiles/cv/tests.py
+++ ProjectFiles/cv/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from cv.models import Project
 
 # Create your tests here.
+ # write Test for Project model
+class ProjectTest(TestCase):
+    def test_str(self):
+        project = Project(name='Test Project')
+        self.assertEqual(str(project), project.name)
+     
