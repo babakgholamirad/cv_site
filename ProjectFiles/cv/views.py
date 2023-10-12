c5b8ccbbe5c68d42783788b4c0c5532a8aa5a692
babakgholamirad <bagholamirad@yahoo.com>
2023-10-12 12:09:41 +0330

    - add LoginRequiredMixin to view classes
    Signed-off-by: babakgholamirad <bagholamirad@yahoo.com>

--- ProjectFiles/cv/views.py
+++ ProjectFiles/cv/views.py
@@ -1,45 +1,50 @@
 from .models import Project
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView, TemplateView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
 # Create your views here.
 
 
+@login_required()
 def home(request):
-
 	return render(request, 'cv/home.html')
 
 
+@login_required()
 def test(request):
-
 	return render(request, 'cv/test.html')
 
-@login_required
-class DashboardView(TemplateView):
+
+class DashboardView(LoginRequiredMixin, TemplateView):
 	template_name = 'accounts/dashboard-base.html'
 
-@login_required
-class ProjectsView(ListView):
+
+class ProjectsView(LoginRequiredMixin, ListView):
 	model = Project
 	template_name = 'accounts/dashboard-projects.html'
 
-@login_required
-class ProjectDetailView(DetailView):
+
+class ProjectDetailView(LoginRequiredMixin, DetailView):
 	model = Project
 	template_name = 'accounts/dashboard-project-detail.html'
 
-@login_required
-class ProjectCreateView(CreateView):
+
+class ProjectCreateView(LoginRequiredMixin, CreateView):
 	model = Project
 	template_name = 'accounts/dashboard-project-add.html'
 	fields = ['start_date']
-	
+
 	def form_valid(self, form):
 		user = self.request.user
 		form.instance.leader = user
 		return super(ProjectCreateView, self).form_valid(form)
+	
+	def get_success_url(self):
+		return reverse('project-detail', kwargs={'pk': self.object.pk})
 
-@login_required
-class ProfileView(TemplateView):
+
+class ProfileView(LoginRequiredMixin, TemplateView):
 	template_name = 'cv/profile_template.html'
